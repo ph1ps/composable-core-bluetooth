7c0f69f4963311b615958541eb04ba1de44eb703
Philipp Gabriel <ph1psdev@gmail.com>
2020-11-04 17:31:27 +0100

    update to latest composable architecture release

--- Package.swift
+++ Package.swift
@@ -18,11 +18,9 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.8.0")
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.9.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ComposableCoreBluetooth",
             dependencies: [
@@ -31,6 +29,9 @@ let package = Package(
         ),
         .testTarget(
             name: "ComposableCoreBluetoothTests",
-            dependencies: ["ComposableCoreBluetooth"]),
+            dependencies: [
+                "ComposableCoreBluetooth"
+            ]
+        ),
     ]
 )
